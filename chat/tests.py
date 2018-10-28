a85c2d41beff6cd5d032deec80a6d0d99f241f04
Tom Liang <tomliangg@gmail.com>
2018-10-28 14:53:41 -0700

    created dummy test for CI config purpose

--- chat/tests.py
+++ chat/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+class DummyTests(TestCase):
+    """
+    Created dummy tests to see if CI is working
+    """
+    def test_dummy(self):
+        self.assertEqual(1, 1)
